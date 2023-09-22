74e7f72b5bfa9b127248619d1ee7f7f082e3e3c0
Trinh Huu Tuan <tuanhuu3264@gmail.com>
2023-09-22 22:08:04 +0700

    update something

--- src/routes/order.ts
+++ src/routes/order.ts
@@ -6,7 +6,7 @@ import { Role } from '../typings/types'
 import { createOrder, getPaginationOrders, getPaginationOrdersAdmin, updateOrder } from '../controllers/order'
 const router = express.Router()
 
-router.get('/pagination', verifyToken, checkRole([Role.Admin]), getPaginationOrders)
+router.get('/pagination', verifyToken, getPaginationOrders)
 router.post('/', verifyToken, checkRole([Role.Admin]), createOrder)
 router.put('/:id', verifyToken, checkRole([Role.Admin]), updateOrder)
 router.get('admin/pagination', verifyToken, checkRole([Role.Admin]), getPaginationOrdersAdmin)
